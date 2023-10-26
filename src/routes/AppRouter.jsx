7c1585f2f91f5d43173616e43839a0abcd411e6a
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-10-26 01:22:07 -0600

    :art:Change route access structure

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import { Route, Switch } from "wouter";
+import { useLocation } from "wouter";
+import { auth, db } from "../services/FirebaseServices";
 import SplashScreen from "../pages/SplashScreen";
 import LoginScreen from "../pages/LoginScreen";
 import HomeScreen from "../pages/HomeScreen";
-import AdminScreen from "../pages/AdminScreen";
-import { auth, db } from "../services/FirebaseServices";
+import GuestScreen from "../pages/GuestScreen";
+import ManagementHardwareScreen from "../pages/ManagementHardwareScreen";
+import ReportHardwareScreen from "../pages/ReportHardwareScreen";
 
 const AppRouter = () => {
     const [isLoggedIn, setLoggedIn] = useState(false);
     const [role, setRole] = useState("");
     const [initialCheckDone, setInitialCheckDone] = useState(false);
+    const [, setLocation] = useLocation();
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(async (currentUser) => {
@@ -35,6 +39,7 @@ const AppRouter = () => {
             await auth.signOut();
             setLoggedIn(false);
             setRole("");
+            setLocation("/login");
 
         } catch (error) {
 
@@ -48,15 +53,31 @@ const AppRouter = () => {
 
     return (
         <Switch>
-            <Route path="/" component={() => <SplashScreen />} />
+            <Route path="/" component={SplashScreen} />
             <Route path="/login">
-                {!isLoggedIn ? <LoginScreen /> : role === "admin" ? <AdminScreen onSignOut={handleSignOut} /> : <LoginScreen />}
+                <LoginScreen onSignOut={isLoggedIn && role === "admin" ? handleSignOut : undefined} />
             </Route>
             <Route path="/home">
-                {role === "invitado" && isLoggedIn ? <HomeScreen onSignOut={handleSignOut} /> : <LoginScreen />}
+                {() => {
+                    if (role === "invitado" && isLoggedIn) {
+                        return <GuestScreen onSignOut={handleSignOut} />;
+                    }
+                    return <LoginScreen />;
+                }}
             </Route>
             <Route path="/admin">
-                {role === "admin" && isLoggedIn ? <AdminScreen onSignOut={handleSignOut} /> : <LoginScreen />}
+                {() => {
+                    if (role === "admin" && isLoggedIn) {
+                        return <HomeScreen onSignOut={handleSignOut} />;
+                    }
+                    return <LoginScreen />;
+                }}
+            </Route>
+            <Route path="/managementHardware">
+                <ManagementHardwareScreen onSignOut={handleSignOut} />
+            </Route>
+            <Route path="/reportHardWare">
+                <ReportHardwareScreen onSignOut={handleSignOut} />
             </Route>
         </Switch>
     );
