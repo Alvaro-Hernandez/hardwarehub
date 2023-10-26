97df5749a22e3fedc3b88d7af14f248596515fe5
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-10-26 01:26:12 -0600

    :lipstick:Change component structure and styles

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,12 +1,29 @@
 import PropTypes from "prop-types";
 import NavBarComponent from "../components/NavbarComponent";
-// import '../styles/homeStyle.css';
+import CardFunction from "../components/CardFunctionComponent";
+import imgGestion from "../assets/gestionHardware.png";
+import imgInforme from "../assets/informeHardware.png";
+import '../styles/homeStyle.css';
 
 const HomeScreen = ({ onSignOut }) => {
     return (
         <div className="homeScreenContainer">
             <div className="navContainer">
                 <NavBarComponent onSignOut={onSignOut} />
+            </div>
+            <div className="itemContainer">
+                <div className="cardsContainer">
+                    <CardFunction
+                        imageSrc={imgGestion}
+                        text="Gestión de Hardware"
+                        route="/managementHardware"
+                    />
+                    <CardFunction
+                        imageSrc={imgInforme}
+                        text="Informe de Hardware"
+                        route="/reportHardWare"
+                    />
+                </div>
             </div>
         </div>
     )
